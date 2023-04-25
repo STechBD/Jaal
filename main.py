48aa51b8e7f7b06de614a6a1935f055348f6f575
Md. Ashraful Alam Shemul <aashemul@gmail.com>
2023-04-25 23:11:47 +0600

    Update

---
+++ main.py
@@ -0,0 +1,7 @@
+import sys
+from urllib.parse import urlparse
+from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QLineEdit, QTabWidget, QMenu, \
+	QMenuBar, QStatusBar, QMessageBox, QToolBar
+from PyQt6.QtGui import QAction, QIcon
+from PyQt6.QtWebEngineWidgets import QWebEngineView
+from PyQt6.QtCore import Qt, QUrl

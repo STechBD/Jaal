3adbfd3e4a05ab1cb732e90655839e4c6516ebe9
Md. Ashraful Alam Shemul <aashemul@gmail.com>
2023-04-26 17:49:35 +0600

    Minor update

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+"""
+Package: PyWeb
+Version: 1.0.0
+Description: PyWeb is a web browser made with Python and PyQt6.
+Author: Md. Ashraful Alam Shemul
+Author URI: https://www.aashemul.com
+Author GitHub: https://github.com/AAShemul
+GitHub Repository: https://github.com/AAShemul/PyWeb
+Created: April 24, 2023
+Updated: April 26, 2023
+License: MIT
+License URI: https://opensource.org/licenses/MIT
+"""
+
 import sys
 from urllib.parse import urlparse
 from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QLineEdit, QTabWidget, QMenu, \
@@ -5,3 +19,12 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout,
 from PyQt6.QtGui import QAction, QIcon
 from PyQt6.QtWebEngineWidgets import QWebEngineView
 from PyQt6.QtCore import Qt, QUrl
+
+
+class PyWeb:
+	# Main class
+
+if __name__ == '__main__':
+	app = QApplication([])
+	window = PyWeb()
+	app.exec()

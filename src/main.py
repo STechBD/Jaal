6c7ea611106c1706585a5ee0f8821069d9943430
Md. Ashraful Alam Shemul <aashemul@gmail.com>
2024-05-07 19:29:16 +0600

    Update

--- src/main.py
+++ src/main.py
@@ -3,9 +3,10 @@ Package: PyWeb
 Version: 1.0.0
 Description: PyWeb is a web browser made with Python and PyQt6.
 Author: Md. Ashraful Alam Shemul
-Author URI: https://www.aashemul.com
 Author GitHub: https://github.com/AAShemul
-GitHub Repository: https://github.com/AAShemul/PyWeb
+Developer: S Technologies
+Developer URI: https://www.stechbd.net
+GitHub Repository: https://github.com/STechBD/PyWeb
 Created: April 24, 2023
 Updated: April 26, 2023
 License: MIT
@@ -14,239 +15,361 @@ License URI: https://opensource.org/licenses/MIT
 
 import sys
 from urllib.parse import urlparse
-from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QLineEdit, QTabWidget, QMenu, \
-	QMenuBar, QStatusBar, QMessageBox, QToolBar
+
+from PyQt6.QtCore import QUrl
 from PyQt6.QtGui import QAction, QIcon
 from PyQt6.QtWebEngineWidgets import QWebEngineView
-from PyQt6.QtCore import Qt, QUrl
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLineEdit, QTabWidget, QMessageBox, QToolBar
 
 
 class PyWeb(QMainWindow):
-	def __init__(self):
-		super().__init__()
+    """
+    The main class of the PyWeb Browser.
 
-		self.tab = None
-		self.url_input = QLineEdit()
-		self.setWindowTitle('PyWeb Browser')
-		self.setWindowIcon(QIcon('image/PyWeb-Logo.webp'))
+    :return: None
+    :since: 1.0.0
+    """
+    def __init__(self):
+        """
+        Constructor function to initialize the PyWeb Browser.
 
-		# Menu Bar
-		self.menu_bar = self.menuBar()
-		self.file_menu = self.menu_bar.addMenu('File')
-		self.edit_menu = self.menu_bar.addMenu('Edit')
-		self.view_menu = self.menu_bar.addMenu('View')
-		self.help_menu = self.menu_bar.addMenu('Help')
+        :return: None
+        :since: 1.0.0
+        """
+        super().__init__()
 
-		# File Menu
-		self.new_tab_action = QAction('New Tab', self)
-		self.new_tab_action.triggered.connect(self.create_tab)
-		self.file_menu.addAction(self.new_tab_action)
+        self.tab = None
+        self.url_input = QLineEdit()
+        self.setWindowTitle('PyWeb Browser')
+        self.setWindowIcon(QIcon('image/PyWeb-Logo.webp'))
 
-		self.bookmarks_action = QAction('Bookmarks', self)
-		self.bookmarks_action.triggered.connect(self.show_bookmarks)
-		self.file_menu.addAction(self.bookmarks_action)
+        # Menu Bar
+        self.menu_bar = self.menuBar()
+        self.file_menu = self.menu_bar.addMenu('File')
+        self.edit_menu = self.menu_bar.addMenu('Edit')
+        self.view_menu = self.menu_bar.addMenu('View')
+        self.help_menu = self.menu_bar.addMenu('Help')
 
-		self.history_action = QAction('History', self)
-		self.history_action.triggered.connect(self.show_history)
-		self.file_menu.addAction(self.history_action)
+        # File Menu
+        self.new_tab_action = QAction('New Tab', self)
+        self.new_tab_action.triggered.connect(self.create_tab)
+        self.file_menu.addAction(self.new_tab_action)
 
-		self.settings_action = QAction('Settings', self)
-		self.settings_action.triggered.connect(self.show_settings)
-		self.file_menu.addAction(self.settings_action)
+        self.bookmarks_action = QAction('Bookmarks', self)
+        self.bookmarks_action.triggered.connect(self.show_bookmarks)
+        self.file_menu.addAction(self.bookmarks_action)
 
-		self.exit_action = QAction('Exit', self)
-		self.exit_action.triggered.connect(exit_browser)
-		self.file_menu.addAction(self.exit_action)
+        self.history_action = QAction('History', self)
+        self.history_action.triggered.connect(self.show_history)
+        self.file_menu.addAction(self.history_action)
 
-		# Edit Menu
-		self.add_bookmark_action = QAction('Add Bookmark', self)
-		self.add_bookmark_action.triggered.connect(self.add_bookmark)
-		self.edit_menu.addAction(self.add_bookmark_action)
+        self.settings_action = QAction('Settings', self)
+        self.settings_action.triggered.connect(self.show_settings)
+        self.file_menu.addAction(self.settings_action)
 
-		self.remove_bookmark_action = QAction('Remove Bookmark', self)
-		self.remove_bookmark_action.triggered.connect(self.remove_bookmark)
-		self.edit_menu.addAction(self.remove_bookmark_action)
+        self.exit_action = QAction('Exit', self)
+        self.exit_action.triggered.connect(exit_browser)
+        self.file_menu.addAction(self.exit_action)
 
-		# View Menu
-		self.toggle_toolbar_action = QAction('Toggle Toolbar', self)
-		self.toggle_toolbar_action.triggered.connect(self.toggle_toolbar)
-		self.view_menu.addAction(self.toggle_toolbar_action)
+        # Edit Menu
+        self.add_bookmark_action = QAction('Add Bookmark', self)
+        self.add_bookmark_action.triggered.connect(self.add_bookmark)
+        self.edit_menu.addAction(self.add_bookmark_action)
 
-		self.back_action = QAction('Back', self)
-		self.back_action.triggered.connect(self.back)
-		self.view_menu.addAction(self.back_action)
+        self.remove_bookmark_action = QAction('Remove Bookmark', self)
+        self.remove_bookmark_action.triggered.connect(self.remove_bookmark)
+        self.edit_menu.addAction(self.remove_bookmark_action)
 
-		self.forward_action = QAction('Forward', self)
-		self.forward_action.triggered.connect(self.forward)
-		self.view_menu.addAction(self.forward_action)
+        # View Menu
+        self.toggle_toolbar_action = QAction('Toggle Toolbar', self)
+        self.toggle_toolbar_action.triggered.connect(self.toggle_toolbar)
+        self.view_menu.addAction(self.toggle_toolbar_action)
 
-		self.reload_action = QAction('Reload', self)
-		self.reload_action.triggered.connect(self.reload)
-		self.view_menu.addAction(self.reload_action)
+        self.back_action = QAction('Back', self)
+        self.back_action.triggered.connect(self.back)
+        self.view_menu.addAction(self.back_action)
 
-		self.stop_action = QAction('Stop', self)
-		# UC # self.stop_action.triggered.connect(self.stop)
-		self.view_menu.addAction(self.stop_action)
+        self.forward_action = QAction('Forward', self)
+        self.forward_action.triggered.connect(self.forward)
+        self.view_menu.addAction(self.forward_action)
 
-		# Help Menu
-		self.about_action = QAction('About', self)
-		self.about_action.triggered.connect(self.about)
-		self.help_menu.addAction(self.about_action)
+        self.reload_action = QAction('Reload', self)
+        self.reload_action.triggered.connect(self.reload)
+        self.view_menu.addAction(self.reload_action)
 
-		# Status Bar
-		self.status_bar = self.statusBar()
-		self.status_bar.showMessage('Welcome')
+        self.stop_action = QAction('Stop', self)
+        # UC # self.stop_action.triggered.connect(self.stop)
+        self.view_menu.addAction(self.stop_action)
 
-		# Tool Bar
-		self.tool_bar = QToolBar()
-		self.addToolBar(self.tool_bar)
+        # Help Menu
+        self.about_action = QAction('About', self)
+        self.about_action.triggered.connect(self.about)
+        self.help_menu.addAction(self.about_action)
 
-		self.back_button = QAction(QIcon('icon/back.svg'), 'Back', self)
-		self.back_button.triggered.connect(self.back)
-		self.tool_bar.addAction(self.back_button)
+        # Status Bar
+        self.status_bar = self.statusBar()
+        self.status_bar.showMessage('Welcome')
 
-		self.forward_button = QAction(QIcon('icon/forward.svg'), 'Forward', self)
-		self.forward_button.triggered.connect(self.forward)
-		self.tool_bar.addAction(self.forward_button)
+        # Tool Bar
+        self.tool_bar = QToolBar()
+        self.addToolBar(self.tool_bar)
 
-		self.reload_button = QAction(QIcon('icon/reload.svg'), 'Reload', self)
-		self.reload_button.triggered.connect(self.reload)
-		self.tool_bar.addAction(self.reload_button)
+        self.back_button = QAction(QIcon('icon/back.svg'), 'Back', self)
+        self.back_button.triggered.connect(self.back)
+        self.tool_bar.addAction(self.back_button)
 
-		self.stop_action = QAction(QIcon('image/stop.png'), 'Stop', self)
-		# UC # self.stop_action.triggered.connect(self.stop)
-		self.tool_bar.addAction(self.stop_action)
+        self.forward_button = QAction(QIcon('icon/forward.svg'), 'Forward', self)
+        self.forward_button.triggered.connect(self.forward)
+        self.tool_bar.addAction(self.forward_button)
 
-		self.home_button = QAction(QIcon('icon/home.svg'), 'Home', self)
-		self.home_button.triggered.connect(self.go_home)
-		self.tool_bar.addAction(self.home_button)
+        self.reload_button = QAction(QIcon('icon/reload.svg'), 'Reload', self)
+        self.reload_button.triggered.connect(self.reload)
+        self.tool_bar.addAction(self.reload_button)
 
-		self.url_input = QLineEdit(self)
-		self.url_input.returnPressed.connect(lambda: self.load_url())
-		self.tool_bar.addWidget(self.url_input)
+        self.stop_action = QAction(QIcon('image/stop.png'), 'Stop', self)
+        # UC # self.stop_action.triggered.connect(self.stop)
+        self.tool_bar.addAction(self.stop_action)
 
-		self.tool_bar.addSeparator()
+        self.home_button = QAction(QIcon('icon/home.svg'), 'Home', self)
+        self.home_button.triggered.connect(self.go_home)
+        self.tool_bar.addAction(self.home_button)
 
-		self.add_bookmark_action = QAction(QIcon('icon/add-bookmark.svg'), 'Add Bookmark', self)
-		self.add_bookmark_action.triggered.connect(self.add_bookmark)
-		self.tool_bar.addAction(self.add_bookmark_action)
+        self.url_input = QLineEdit(self)
+        self.url_input.returnPressed.connect(lambda: self.load_url())
+        self.tool_bar.addWidget(self.url_input)
 
-		self.remove_bookmark_action = QAction(QIcon('icon/remove-bookmark.svg'), 'Remove Bookmark', self)
-		self.remove_bookmark_action.triggered.connect(self.remove_bookmark)
-		self.tool_bar.addAction(self.remove_bookmark_action)
+        self.tool_bar.addSeparator()
 
-		# Tab Widget
-		self.tabs = QTabWidget()
-		self.tabs.setTabsClosable(True)
-		self.tabs.tabCloseRequested.connect(self.close_tab)
-		self.setCentralWidget(self.tabs)
+        self.add_bookmark_action = QAction(QIcon('icon/add-bookmark.svg'), 'Add Bookmark', self)
+        self.add_bookmark_action.triggered.connect(self.add_bookmark)
+        self.tool_bar.addAction(self.add_bookmark_action)
 
-		# History List
-		self.history_list = []
-		self.create_tab()
-		self.show()
+        self.remove_bookmark_action = QAction(QIcon('icon/remove-bookmark.svg'), 'Remove Bookmark', self)
+        self.remove_bookmark_action.triggered.connect(self.remove_bookmark)
+        self.tool_bar.addAction(self.remove_bookmark_action)
 
-	# Create Tab
-	def create_tab(self):
-		self.tab = QWebEngineView()
-		self.tab.load(QUrl('https://www.stechbd.net'))
-		self.tab.titleChanged.connect(lambda title: self.tabs.setTabText(self.tabs.indexOf(self.tab), title))
-		self.tab.loadStarted.connect(self.tab_load_started)
-		self.tab.loadFinished.connect(self.tab_load_finished)
-		self.tabs.setCurrentWidget(self.tab)
+        # Tab Widget
+        self.tabs = QTabWidget()
+        self.tabs.setTabsClosable(True)
+        self.tabs.tabCloseRequested.connect(self.close_tab)
+        self.setCentralWidget(self.tabs)
 
-	def tab_load_started(self):
-		self.status_bar.showMessage('Loading ...')
+        # History List
+        self.history_list = []
+        self.create_tab()
+        self.show()
 
-	def tab_load_finished(self):
-		self.status_bar.showMessage('Page is ready')
-		url = self.tab.url().toString()
+    # Create Tab
+    def create_tab(self):
+        """
+        Function to create a new tab.
 
-		# UC # Add Favicon
+        :return: None
+        :since: 1.0.0
+        """
+        self.tab = QWebEngineView()
+        self.tab.load(QUrl('https://www.stechbd.net'))
+        self.tab.titleChanged.connect(lambda title: self.tabs.setTabText(self.tabs.indexOf(self.tab), title))
+        self.tab.loadStarted.connect(self.tab_load_started)
+        self.tab.loadFinished.connect(self.tab_load_finished)
+        self.tabs.setCurrentWidget(self.tab)
 
-		# Update the Address Bar
-		self.url_input.setText(url)
+    def tab_load_started(self):
+        """
+        Function to show a message in the status bar when a tab is loading.
 
-		# Add Current URL to the History
-		if url not in self.history_list:
-			self.history_list.append(url)
+        :return: None
+        :since: 1.0.0
+        """
+        self.status_bar.showMessage('Loading ...')
 
-	def add_bookmark(self):
-		# Display the Bookmark List in a Message Box
-		message_box = QMessageBox()
-		# UC # message_box.setText("\n".join(self.bookmark_list))
-		message_box.exec()
+    def tab_load_finished(self):
+        """
+        Function to show a message in the status bar when a tab is loaded.
 
-	def remove_bookmark(self):
-		# Display the Bookmark List in a Message Box
-		message_box = QMessageBox()
-		# UC # message_box.setText("\n".join(self.bookmark_list))
-		message_box.exec()
+        :return: None
+        :since: 1.0.0
+        """
+        self.status_bar.showMessage('Page is ready')
+        url = self.tab.url().toString()
 
-	def show_bookmarks(self):
-		# Display the Bookmark List in a Message Box
-		message_box = QMessageBox()
-		# UC # message_box.setText("\n".join(self.bookmark_list))
-		message_box.exec()
+        # UC # Add Favicon
 
-	def show_settings(self):
-		# Display the Bookmark List in a Message Box
-		message_box = QMessageBox()
-		# UC # message_box.setText("\n".join(self.bookmark_list))
-		message_box.exec()
+        # Update the Address Bar
+        self.url_input.setText(url)
 
-	def show_history(self):
-		# Display the History List in a Message Box
-		message_box = QMessageBox()
-		message_box.setText("\n".join(self.history_list))
-		message_box.exec()
+        # Add Current URL to the History
+        if url not in self.history_list:
+            self.history_list.append(url)
 
-	def close_tab(self, index):
-		self.tabs.removeTab(index)
+    def add_bookmark(self):
+        """
+        Function to add a bookmark.
 
-		# If there is no tab left, create a new one
-		if self.tabs.count() == 0:
-			self.create_tab()
+        :return: None
+        :since: 1.0.0
+        """
+        # Display the Bookmark List in a Message Box
+        message_box = QMessageBox()
+        # UC # message_box.setText("\n".join(self.bookmark_list))
+        message_box.exec()
 
-	def about(self):
-		QMessageBox.about(self, 'About PyWeb Browser', 'PyWeb is a desktop app for browsing websites.')
+    def remove_bookmark(self):
+        """
+        Function to remove a bookmark.
 
-	def back(self):
-		if self.tab:
-			self.tab.back()
+        :return: None
+        :since: 1.0.0
+        """
+        # Display the Bookmark List in a Message Box
+        message_box = QMessageBox()
+        # UC # message_box.setText("\n".join(self.bookmark_list))
+        message_box.exec()
 
-	def forward(self):
-		if self.tab:
-			self.tab.forward()
+    def show_bookmarks(self):
+        """
+        Function to show bookmarks.
 
-	def reload(self):
-		if self.tab:
-			self.tab.reload()
+        :return: None
+        :since: 1.0.0
+        """
+        # Display the Bookmark List in a Message Box
+        message_box = QMessageBox()
+        # UC # message_box.setText("\n".join(self.bookmark_list))
+        message_box.exec()
 
-	def go_home(self):
-		if self.tab:
-			self.tab.load(QUrl('https://www.stechbd.net'))
+    def show_settings(self):
+        """
+        Function to show settings.
 
-	def load_url(self):
-		url = self.url_input.text().strip()
-		if url:
-			if not urlparse(url).scheme:
-				url = 'https://' + url
+        :return: None
+        :since: 1.0.0
+        """
+        # Display the Bookmark List in a Message Box
+        message_box = QMessageBox()
+        # UC # message_box.setText("\n".join(self.bookmark_list))
+        message_box.exec()
 
-		self.tab.load(QUrl(url))
+    def show_history(self):
+        """
+        Function to show history.
 
-	def toggle_toolbar(self, checked):
-		if checked:
-			self.tool_bar.show()
-		else:
-			self.tool_bar.hide()
+        :return: None
+        :since: 1.0.0
+        """
+        # Display the History List in a Message Box
+        message_box = QMessageBox()
+        message_box.setText("\n".join(self.history_list))
+        message_box.exec()
+
+    def close_tab(self, index):
+        """
+        Function to close a tab.
+
+        :param index: The index of the tab to be closed.
+        :return: None
+        :since: 1.0.0
+        """
+        self.tabs.removeTab(index)
+
+        # If there is no tab left, create a new one
+        if self.tabs.count() == 0:
+            self.create_tab()
+
+    def about(self):
+        """
+        Function to show the About dialog.
+
+        :return: None
+        :since: 1.0.0
+        """
+        QMessageBox.about(self, 'About PyWeb Browser', 'PyWeb is a desktop app for browsing websites.')
+
+    def back(self):
+        """
+        Function to go back to the previous page.
+
+        :return: None
+        :since: 1.0.0
+        """
+        if self.tab:
+            self.tab.back()
+
+    def forward(self):
+        """
+        Function to go forward to the next page.
+
+        :return: None
+        :since: 1.0.0
+        """
+        if self.tab:
+            self.tab.forward()
+
+    def reload(self):
+        """
+        Function to reload the current page.
+
+        :return: None
+        :since: 1.0.0
+        """
+        if self.tab:
+            self.tab.reload()
+
+    def go_home(self):
+        """
+        Function to go to the home page.
+
+        :return: None
+        :since: 1.0.0
+        """
+        if self.tab:
+            self.tab.load(QUrl('https://www.stechbd.net'))
+
+    def load_url(self):
+        """
+        Function to load the URL in the address bar.
+
+        :return: None
+        :since: 1.0.0
+        """
+        url = self.url_input.text().strip()
+        if url:
+            if not urlparse(url).scheme:
+                url = 'https://' + url
+
+        self.tab.load(QUrl(url))
+
+    def toggle_toolbar(self, checked):
+        """
+        Function to toggle the toolbar.
+
+        :param checked: The state of the toolbar.
+        :return: None
+        :since: 1.0.0
+        """
+        if checked:
+            self.tool_bar.show()
+        else:
+            self.tool_bar.hide()
 
 
 def exit_browser():
-	sys.exit()
+    """
+    Function to exit the browser.
+
+    :return: None
+    :since: 1.0.0
+    """
+    sys.exit()
 
 
+"""
+The main function to run the PyWeb Browser.
+
+:return: None
+:since: 1.0.0
+"""
 if __name__ == '__main__':
-	app = QApplication([])
-	window = PyWeb()
-	app.exec()
+    app = QApplication([])
+    window = PyWeb()
+    app.exec()
